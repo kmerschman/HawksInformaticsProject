2d71192a55e4ea28adaf95e33c72b046e1f82229
Kaisha <Kaisha@172.17.121.18>
2018-04-05 10:38:07 -0500

    project

--- infoPro_DB.sql
+++ infoPro_DB.sql
@@ -10,7 +10,7 @@ CREATE TABLE course   (
         courseTime              TIME,
 	PRIMARY KEY (courseNumber));
 
-INSERT INTO course  (courseNumber, studentsEnrolled, courseDate, courseTime) VALUES  ('', '', '', '');
+
 
 -- tutor table
 CREATE TABLE tutor   (
@@ -19,18 +19,14 @@ CREATE TABLE tutor   (
 	FOREIGN KEY (courseNumber) REFERENCES course(courseNumber),
 	PRIMARY KEY (hawkid));
 
-INSERT INTO tutor  (hawkid, courseNumber) VALUES  ('', '');
 
 -- tutorAvailability table
 CREATE TABLE tutorAvailability   (
         calendar_id            VARCHAR(10) NOT NULL,
-        sessionDate            DATE,
-        sessionTime            TIME,
-	FOREIGN KEY (sessionDate) REFERENCES sessionSchedule(sessionDate),
-	FOREIGN KEY (sessionTime) REFERENCES sessionSchedule(sessionTime),
+        sessionDateTime           DATETIME,
+	FOREIGN KEY (sessionDateTime) REFERENCES sessionSchedule(sessionDateTime),
 	PRIMARY KEY (calendar_id));
 
-INSERT INTO tutorAvailability  (calendar_id, sessionDate, sessionTime) VALUES  ('', '', '');
 
 
 -- tutorSession table
@@ -46,41 +42,47 @@ CREATE TABLE tutorSession   (
 	FOREIGN KEY (courseNumber) REFERENCES course(courseNumber),
 	PRIMARY KEY (session_id));
 
-INSERT INTO tutorSession  (session_id, hawkid, hawkid, courseNumber, studentCancel, tutorCancel) VALUES  ('', '', '', '', '', '');
+
 
 -- sessionSchedule table
 CREATE TABLE sessionSchedule   (
         session_id              VARCHAR(10) PRIMARY KEY NOT NULL,
-        sessionDate             date,
-        sessionTime             time
+        sessionDateTime         DATETIME,
         courseNumber            VARCHAR(6)  FOREIGN KEY NOT NULL,
         open                    CHAR(1);)
 
-INSERT INTO sessionSchedule  (session_id, sessionDate, sessionTime, courseNumber, open) VALUES  ('', '', '', '', '');
+
 
 
 -- faculty table
 CREATE TABLE faculty   (
-        hawkid(faculty)         VARCHAR(10) PRIMARY KEY NOT NULL,
-        courseNumber            VARCHAR(6)  FOREIGN KEY NOT NULL;)
+        hawkid         VARCHAR(10) PRIMARY KEY NOT NULL,
+        courseNumber   VARCHAR(6)  FOREIGN KEY NOT NULL;)
 
-INSERT INTO faculty  (hawkid, courseNumber) VALUES  ('', '');
+
 
 
 -- student table
 CREATE TABLE student   (
         hawkid                  VARCHAR(10) PRIMARY KEY NOT NULL,
         courseNumber            VARCHAR(6) FOREIGN KEY NOT NULL,
-	      tutorCreditsRemaining   INTEGER,
+	tutorCreditsRemaining   INTEGER,
 
-INSERT INTO student  (hawkid, courseNumber, tutorCreditsRemaining) VALUES  ('', '', '');
+
 
 
 
 -- problemSet table
 CREATE TABLE problemSet   (
         problemNumber           INTEGER PRIMARY KEY NOT NULL,
-	      question                VARCHAR2(400) NOT NULL,
+	      question                VARCHAR(400) NOT NULL,
 	      courseNumber            VARCHAR(6));
 
 INSERT INTO course  (problemNumber, question, courseNumber) VALUES  ('', '', '');
+INSERT INTO course  (courseNumber, studentsEnrolled, courseDate, courseTime) VALUES  ('', '', '', '');
+INSERT INTO student  (hawkid, courseNumber, tutorCreditsRemaining) VALUES  ('', '', '');
+INSERT INTO faculty  (hawkid, courseNumber) VALUES  ('', '');
+INSERT INTO sessionSchedule  (session_id, sessionDateTime, courseNumber, open) VALUES  ('', '', '', '', '');
+INSERT INTO tutorSession  (session_id, hawkid, hawkid, courseNumber, studentCancel, tutorCancel) VALUES  ('', '', '', '', '', '');
+INSERT INTO tutorAvailability  (calendar_id, sessionDateTime) VALUES  ('', '', '');
+INSERT INTO tutor  (hawkid, courseNumber) VALUES  ('', '');
