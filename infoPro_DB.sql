bedd544a416a9a38113683ba78ed191253431972
Kaisha <Kaisha@172.17.121.18>
2018-04-05 13:07:13 -0500

    project

--- infoPro_DB.sql
+++ infoPro_DB.sql
@@ -1,31 +1,54 @@
 -- course table
+DROP TABLE problemSet;
+DROP TABLE admins;
+DROP TABLE faculty;
+DROP TABLE tutorSession;
+DROP TABLE student;
+DROP TABLE tutorAvailability;
+DROP TABLE sessionSchedule;
 DROP TABLE tutor;
 DROP TABLE course;
 
 CREATE TABLE course   (
         courseNumber            VARCHAR(6) NOT NULL,
-	      studentsEnrolled        VARCHAR(25) NOT NULL,
-	      courseDate              DATE,
-        courseTime              TIME,
+	studentsEnrolled        VARCHAR(25) NOT NULL,
+        courseDateTime          DATETIME NOT NULL,
 	PRIMARY KEY (courseNumber));
-
 
 
 -- tutor table
 CREATE TABLE tutor   (
-        hawkid                 VARCHAR(20),
+        hawkidTutor            VARCHAR(20),
         courseNumber           VARCHAR(6),
-	FOREIGN KEY (courseNumber)    REFERENCES course(courseNumber),
-	PRIMARY KEY (hawkid));
+	FOREIGN KEY (courseNumber) REFERENCES course(courseNumber),
+	PRIMARY KEY (hawkidTutor));
+
+
+-- sessionSchedule table
+CREATE TABLE sessionSchedule   (
+        session_id              VARCHAR(10) NOT NULL,
+        sessionDateTime         DATETIME NOT NULL,
+        courseNumber            VARCHAR(6) NOT NULL,
+        openSession             CHAR(1),
+        FOREIGN KEY (courseNumber) REFERENCES course (courseNumber),
+        PRIMARY KEY (session_id));
 
 
 -- tutorAvailability table
 CREATE TABLE tutorAvailability   (
         calendar_id            VARCHAR(10) NOT NULL,
-        sessionDateTime           DATETIME,
+        sessionDateTime        DATETIME NOT NULL,
 	FOREIGN KEY (sessionDateTime) REFERENCES sessionSchedule(sessionDateTime),
 	PRIMARY KEY (calendar_id));
 
+
+-- student table
+CREATE TABLE student   (
+        hawkidStudent           VARCHAR(20) NOT NULL,
+        courseNumber            VARCHAR(6) NOT NULL,
+	tutorCreditsRemaining   INTEGER,
+        FOREIGN KEY (courseNumber) REFERENCES course(courseNumber),
+        PRIMARY KEY (hawkidStudent));
 
 
 -- tutorSession table
@@ -36,47 +59,32 @@ CREATE TABLE tutorSession   (
         courseNumber            VARCHAR(6)  NOT NULL,
         studentCancel           CHAR(1),
         tutorCancel             CHAR(1),
-  FOREIGN KEY (hawkidStudent) REFERENCES student(hawkid);
-	FOREIGN KEY (hawkidTutor) REFERENCES tutor(hawkid);
+        FOREIGN KEY (hawkidStudent) REFERENCES student(hawkidStudent);
+	FOREIGN KEY (hawkidTutor) REFERENCES tutor(hawkidTutor);
 	FOREIGN KEY (courseNumber) REFERENCES course(courseNumber),
 	PRIMARY KEY (session_id));
 
 
-
--- sessionSchedule table
-CREATE TABLE sessionSchedule   (
-        session_id              VARCHAR(10) PRIMARY KEY NOT NULL,
-        sessionDateTime         DATETIME,
-        courseNumber            VARCHAR(6)  FOREIGN KEY NOT NULL,
-        open                    CHAR(1));
-
-
 -- faculty table
 CREATE TABLE faculty   (
-        hawkidFaculty           VARCHAR(20) PRIMARY KEY NOT NULL,
-        courseNumber            VARCHAR(6)  FOREIGN KEY NOT NULL,
-  FOREIGN KEY (hawkidFaculty) REFERENCES role(hawkid));
+        hawkidFaculty           VARCHAR(20) NOT NULL,
+        courseNumber            VARCHAR(6) NOT NULL,
+        FOREIGN KEY (courseNumber) REFERENCES course (courseNumber),
+        PRIMARY KEY (hawkidFaculty));
 
 -- admin table
-CREATE TABLE admin   (
-        hawkidAdmin             VARCHAR(20) PRIMARY KEY NOT NULL,
-  FOREIGN KEY (hawkidAdmin) REFERENCES role(hawkid));
+CREATE TABLE admins   (
+        hawkidAdmin             VARCHAR(20) NOT NULL,
+        PRIMARY KEY (hawkidAdmin));
 
-
--- student table
-CREATE TABLE student   (
-        hawkidStudent           VARCHAR(20) PRIMARY KEY NOT NULL,
-        courseNumber            VARCHAR(6) FOREIGN KEY NOT NULL,
-	      tutorCreditsRemaining   INTEGER,
-  FOREIGN KEY (courseNumber) REFERENCES course(courseNumber),
-  FOREIGN KEY (hawkidStudent) REFERENCES role(hawkid));
 
 
 -- problemSet table
 CREATE TABLE problemSet   (
-        problemNumber           INTEGER PRIMARY KEY NOT NULL,
-	      question                VARCHAR(400) NOT NULL,
-	      courseNumber            VARCHAR(6));
+        problemNumber           INTEGER NOT NULL,
+	question                VARCHAR(400) NOT NULL,
+	courseNumber            VARCHAR(6),
+        PRIMARY KEY (problemNumber));
 
 
 
@@ -91,24 +99,24 @@ INSERT INTO course  (problemNumber, question, courseNumber) VALUES  ('5', 'what 
 INSERT INTO course  (problemNumber, question, courseNumber) VALUES  ('6', 'what is the bootstrap used for?', 'CS:1020');
 
 
-INSERT INTO student  (hawkid, courseNumber, tutorCreditsRemaining) VALUES  ('a', 'CS:1210', '20');
-INSERT INTO student  (hawkid, courseNumber, tutorCreditsRemaining) VALUES  ('b', 'CS:1110', '60');
-INSERT INTO student  (hawkid, courseNumber, tutorCreditsRemaining) VALUES  ('c', 'CS:1020', '40');
-INSERT INTO student  (hawkid, courseNumber, tutorCreditsRemaining) VALUES  ('d', 'CS:1020', '60');
-INSERT INTO student  (hawkid, courseNumber, tutorCreditsRemaining) VALUES  ('e', 'CS:1110', '60');
-INSERT INTO student  (hawkid, courseNumber, tutorCreditsRemaining) VALUES  ('f', 'CS:1210', '20');
-INSERT INTO student  (hawkid, courseNumber, tutorCreditsRemaining) VALUES  ('g', 'CS:1110', '60');
-INSERT INTO student  (hawkid, courseNumber, tutorCreditsRemaining) VALUES  ('h', 'CS:1210', '40');
+INSERT INTO student  (hawkidStudent, courseNumber, tutorCreditsRemaining) VALUES  ('a', 'CS:1210', '20');
+INSERT INTO student  (hawkidStudent, courseNumber, tutorCreditsRemaining) VALUES  ('b', 'CS:1110', '60');
+INSERT INTO student  (hawkidStudent, courseNumber, tutorCreditsRemaining) VALUES  ('c', 'CS:1020', '40');
+INSERT INTO student  (hawkidStudent, courseNumber, tutorCreditsRemaining) VALUES  ('d', 'CS:1020', '60');
+INSERT INTO student  (hawkidStudent, courseNumber, tutorCreditsRemaining) VALUES  ('e', 'CS:1110', '60');
+INSERT INTO student  (hawkidStudent, courseNumber, tutorCreditsRemaining) VALUES  ('f', 'CS:1210', '20');
+INSERT INTO student  (hawkidStudent, courseNumber, tutorCreditsRemaining) VALUES  ('g', 'CS:1110', '60');
+INSERT INTO student  (hawkidStudent, courseNumber, tutorCreditsRemaining) VALUES  ('h', 'CS:1210', '40');
 
 
-INSERT INTO faculty  (hawkid, courseNumber) VALUES  ('jphourcade', 'CS:1210');
-INSERT INTO faculty  (hawkid, courseNumber) VALUES  ('amsegre', 'CS:1020');
-INSERT INTO faculty  (hawkid, courseNumber) VALUES  ('rparida', 'CS:1110');
+INSERT INTO faculty  (hawkidFaculty, courseNumber) VALUES  ('jphourcade', 'CS:1210');
+INSERT INTO faculty  (hawkidFaculty, courseNumber) VALUES  ('amsegre', 'CS:1020');
+INSERT INTO faculty  (hawkidFaculty, courseNumber) VALUES  ('rparida', 'CS:1110');
 
 
-INSERT INTO sessionSchedule  (session_id, sessionDateTime, courseNumber, open) VALUES  ('1', '', 'CS:1210', 'Y');
-INSERT INTO sessionSchedule  (session_id, sessionDateTime, courseNumber, open) VALUES  ('2', '', 'CS:1110', 'Y');
-INSERT INTO sessionSchedule  (session_id, sessionDateTime, courseNumber, open) VALUES  ('3', '', 'CS:1020', 'N');
+INSERT INTO sessionSchedule  (session_id, sessionDateTime, courseNumber, openSession) VALUES  ('1', '', 'CS:1210', 'Y');
+INSERT INTO sessionSchedule  (session_id, sessionDateTime, courseNumber, openSession) VALUES  ('2', '', 'CS:1110', 'Y');
+INSERT INTO sessionSchedule  (session_id, sessionDateTime, courseNumber, openSession) VALUES  ('3', '', 'CS:1020', 'N');
 
 
 INSERT INTO tutorSession  (session_id, hawkidStudent, hawkidTutor, courseNumber, studentCancel, tutorCancel) VALUES  ('1', 'a', 'kmerschman', 'CS:1210', '', '');
@@ -122,11 +130,11 @@ INSERT INTO tutorAvailability  (calendar_id, sessionDateTime) VALUES  ('3', '');
 INSERT INTO tutorAvailability  (calendar_id, sessionDateTime) VALUES  ('4', '');
 
 
-INSERT INTO tutor  (hawkid, courseNumber) VALUES  ('kmerschman', 'CS:1210');
-INSERT INTO tutor  (hawkid, courseNumber) VALUES  ('adanover', 'CS:1110');
-INSERT INTO tutor  (hawkid, courseNumber) VALUES  ('kmbillings', 'CS:1020');
+INSERT INTO tutor  (hawkidTutor, courseNumber) VALUES  ('kmerschman', 'CS:1210');
+INSERT INTO tutor  (hawkidTutor, courseNumber) VALUES  ('adanover', 'CS:1110');
+INSERT INTO tutor  (hawkidTutor, courseNumber) VALUES  ('kmbillings', 'CS:1020');
 
 
-INSERT INTO admin  (hawkid) VALUES  ('kmerschman');
-INSERT INTO admin  (hawkid) VALUES  ('adanover');
-INSERT INTO admin  (hawkid) VALUES  ('kmbillings');
+INSERT INTO admins  (hawkidAdmin) VALUES  ('kmerschman');
+INSERT INTO admins  (hawkidAdmin) VALUES  ('adanover');
+INSERT INTO admins  (hawkidAdmin) VALUES  ('kmbillings');
