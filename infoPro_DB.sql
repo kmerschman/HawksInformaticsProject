bce7fe4f0e06ce488345797b435e64b58ad6f9df
Keaton Merschman <keaton-merschman@uiowa.edu>
2018-04-09 13:01:00 -0500

    git atom test

--- infoPro_DB.sql
+++ infoPro_DB.sql
@@ -98,7 +98,6 @@ INSERT INTO problemSet  (problemNumber, question, courseNumber) VALUES  ('4', 'w
 INSERT INTO problemSet  (problemNumber, question, courseNumber) VALUES  ('5', 'what is the difference between html and html5?', 'CS:1210');
 INSERT INTO problemSet  (problemNumber, question, courseNumber) VALUES  ('6', 'what is the bootstrap used for?', 'CS:1020');
 
-
 INSERT INTO student  (hawkidStudent, courseNumber, tutorCreditsRemaining) VALUES  ('a', 'CS:1210', '20');
 INSERT INTO student  (hawkidStudent, courseNumber, tutorCreditsRemaining) VALUES  ('b', 'CS:1110', '60');
 INSERT INTO student  (hawkidStudent, courseNumber, tutorCreditsRemaining) VALUES  ('c', 'CS:1020', '40');
@@ -108,32 +107,26 @@ INSERT INTO student  (hawkidStudent, courseNumber, tutorCreditsRemaining) VALUES
 INSERT INTO student  (hawkidStudent, courseNumber, tutorCreditsRemaining) VALUES  ('g', 'CS:1110', '60');
 INSERT INTO student  (hawkidStudent, courseNumber, tutorCreditsRemaining) VALUES  ('h', 'CS:1210', '40');
 
-
 INSERT INTO faculty  (hawkidFaculty, courseNumber) VALUES  ('jphourcade', 'CS:1210');
 INSERT INTO faculty  (hawkidFaculty, courseNumber) VALUES  ('amsegre', 'CS:1020');
 INSERT INTO faculty  (hawkidFaculty, courseNumber) VALUES  ('rparida', 'CS:1110');
-
 
 INSERT INTO sessionSchedule  (session_id, sessionDateTime, courseNumber, openSession) VALUES  ('1', '', 'CS:1210', 'Y');
 INSERT INTO sessionSchedule  (session_id, sessionDateTime, courseNumber, openSession) VALUES  ('2', '', 'CS:1110', 'Y');
 INSERT INTO sessionSchedule  (session_id, sessionDateTime, courseNumber, openSession) VALUES  ('3', '', 'CS:1020', 'N');
 
-
 INSERT INTO tutorSession  (session_id, hawkidStudent, hawkidTutor, courseNumber, studentCancel, tutorCancel) VALUES  ('1', 'a', 'kmerschman', 'CS:1210', '', '');
 INSERT INTO tutorSession  (session_id, hawkidStudent, hawkidTutor, courseNumber, studentCancel, tutorCancel) VALUES  ('2', 'b', 'adanover', 'CS:1110', '', '');
 INSERT INTO tutorSession  (session_id, hawkidStudent, hawkidTutor, courseNumber, studentCancel, tutorCancel) VALUES  ('3', 'c', 'kmbillings', 'CS:1020', '', '');
-
 
 INSERT INTO tutorAvailability  (calendar_id, sessionDateTime) VALUES  ('1', '');
 INSERT INTO tutorAvailability  (calendar_id, sessionDateTime) VALUES  ('2', '');
 INSERT INTO tutorAvailability  (calendar_id, sessionDateTime) VALUES  ('3', '');
 INSERT INTO tutorAvailability  (calendar_id, sessionDateTime) VALUES  ('4', '');
 
-
 INSERT INTO tutor  (hawkidTutor, courseNumber) VALUES  ('kmerschman', 'CS:1210');
 INSERT INTO tutor  (hawkidTutor, courseNumber) VALUES  ('adanover', 'CS:1110');
 INSERT INTO tutor  (hawkidTutor, courseNumber) VALUES  ('kmbillings', 'CS:1020');
-
 
 INSERT INTO admins  (hawkidAdmin) VALUES  ('kmerschman');
 INSERT INTO admins  (hawkidAdmin) VALUES  ('adanover');
