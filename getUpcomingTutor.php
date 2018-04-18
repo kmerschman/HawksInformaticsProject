100e4ed8e760f5351fa902bd5b1fff9d72544d43
Kaisha <Kaisha@192.168.2.9>
2018-04-18 14:48:06 -0500

    Updates

---
+++ getUpcomingTutor.php
@@ -0,0 +1,30 @@
+<?php
+
+// We need to include these two files in order to work with the database
+include_once('config.php');
+include_once('dbutils.php');
+
+$db = connectDB($DBHost, $DBUser, $DBPassword, $DBName);
+session_start();
+$username = $_SESSION['username'];
+
+$query = "SELECT * FROM tutorSlots WHERE hawkidTutor = '$username' AND hawkidStudent IS NOT NULL;";
+
+$result = queryDB($query, $db);
+
+$sessions = array();
+$i = 0;
+
+while ($thissession = nextTuple($result)) {
+    $sessions[$i] = $thissession;
+    $i++;
+};
+
+//create JSON object for movies
+$response = array();
+$response['status'] = 'success';
+$response['value']['sessions'] = $sessions;
+header('Content-Type: application/json');
+echo(json_encode($response));
+?>
+
