a4066c5f72b998d06a3fdc9235d842ca22f7ea9e
Kaisha <Kaisha@192.168.2.9>
2018-04-24 10:09:21 -0500

    Update credits to reflect canceled appointment

--- cancelSession.php
+++ cancelSession.php
@@ -9,8 +9,12 @@ $db = connectDB($DBHost, $DBUser, $DBPassword, $DBName);
 $data = json_decode(file_get_contents('php://input'), true);
 $session_id = $data['session_id'];
 
+
 $query = "UPDATE tutorSlots SET hawkidStudent = NULL WHERE session_id = $session_id;";
 queryDB($query, $db);
+$query2 ="UPDATE studentCourse SET credits = (credits +1) WHERE courseNumber = (SELECT courseNumber FROM tutorSlot WHERE session_id = $session_id;);";
+queryDB($query2, $db);
+
 $response = array();
 $response['status'] = 'success';
 header('Content-Type: application/json');
